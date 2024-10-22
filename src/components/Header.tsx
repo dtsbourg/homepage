4f0c4db91726852493b56a4755488deef0d689b1
Dylan Bourgeois <dtsbourg@gmail.com>
2024-10-22 13:53:28 -0400

    update projects link footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -123,7 +123,7 @@ function MobileNavigation(
           <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
             <MobileNavItem href="/">Home</MobileNavItem>
             <MobileNavItem href="/about">About</MobileNavItem>
-            <MobileNavItem href="/articles">Articles</MobileNavItem>
+            <MobileNavItem href="/articles">Thoughts</MobileNavItem>
             {/* <MobileNavItem href="/projects">Projects</MobileNavItem> */}
             <MobileNavItem href="/research">Research</MobileNavItem>
           </ul>
